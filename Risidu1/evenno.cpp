d5be4095a1f62aec708dd5b51a186d0db82808ff
Risidu03 <geeganagerisidu@gmail.com>
2025-03-06 14:58:52 +0530

    Add files via upload

---
+++ Risidu1/evenno.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	
+	cout<<"Enter a number:-";
+	cin>>num;
+	
+	if  (num %2 == 0){
+		cout<<"Even number."<<endl;
+	}else {
+		cout<<"ODD number,"<<endl;
+	}
+
+return 0;
+}
